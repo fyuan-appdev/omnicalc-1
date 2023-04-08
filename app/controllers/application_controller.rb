f82b97c8b7d1b95034036312c2cf79cb80b2afb8
Finch Yuan <fyuan@chicagobooth.edu>
2023-04-08 02:48:48 +0000

    Done with Square

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  
+  def blank_square_form
+    render ({:template =>"calculation_templates/square_form.html.erb"})
+  end
+
+  def square_result
+    @number = params.fetch("number").to_f
+    @square = (@number*@number).round(2)
+    render ({:template =>"calculation_templates/square_result.html.erb"})
+  end
 
 end
